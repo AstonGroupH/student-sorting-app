f1425cd48fb85b8d1fa62f5b2afefe63c1c208a2
Михеев Максим Павлович <maksim.mikheev@rubius.com>
2026-07-28 19:21:50 +0300

    Update tests

--- src/test/java/StudentBuilderTest.java
+++ src/test/java/StudentBuilderTest.java
@@ -1,16 +1,41 @@
 import org.astongrouph.model.Student;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class StudentBuilderTest {
 
     @Test
     public void whenBuildingStudentWithValidData_thenFieldsAreSetCorrectly() {
-        assert(true);
+        var student = new Student.Builder(12, 100.1, 10099).build();
+        String result = "Student: [ group number : 12 ] [ average score : 100,10 ] [ record book number : 10099 ]";
+
+        assert (student.toString().equals(result));
     }
 
     @Test
     public void whenBuildingStudentWithNegativeGroupNumber_thenThrowException() {
-        assert(true);
+        try {
+            var student = new Student.Builder(-12, 100.1, 10099).build();
+        } catch (Exception e) {
+            assert(e.getMessage().equals("group number must be > 0"));
+        }
     }
+
+    @Test
+    public void whenBuildingStudentWithNegativeAverageScore_thenThrowException() {
+        try {
+            var student = new Student.Builder(12, -100.1, 10099).build();
+        } catch (Exception e) {
+            assert(e.getMessage().equals("average score must be > 0"));
+        }
+    }
+
+    @Test
+    public void whenBuildingStudentWithNegativeRecordBookNumber_thenThrowException() {
+        try {
+            var student = new Student.Builder(12, 100.1, -10099).build();
+        } catch (Exception e) {
+            assert(e.getMessage().equals("record book number must be > 0"));
+        }
+    }
+
 }
