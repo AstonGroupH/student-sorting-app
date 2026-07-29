6f06a07f447fba1cf117a9753e0726aa2a312fed
Михеев Максим Павлович <maksim.mikheev@rubius.com>
2026-07-29 17:20:45 +0300

    Integrated Menu in main

--- src/main/java/org/astongrouph/Main.java
+++ src/main/java/org/astongrouph/Main.java
@@ -1,15 +1,10 @@
 package org.astongrouph;
 
-import org.astongrouph.model.Student;
+import org.astongrouph.menu.Menu;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            var student = new Student.Builder(12, 100.1, 10099).build();
-            System.out.println("Student build: " + student);
-        }
-        catch (Exception e) {
-            System.err.println("Error student build: " + e.getMessage());
-        }
+        Menu menu = new Menu();
+        menu.show();
     }
 }
