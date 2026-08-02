871315a4e7487691ef769845e2124bb8c3b4a45d
Михеев Максим Павлович <maksim.mikheev@rubius.com>
2026-08-02 18:42:22 +0300

    ShowInfoHeader moved individual module

--- src/main/java/org/astongrouph/operations/Operations.java
+++ src/main/java/org/astongrouph/operations/Operations.java
@@ -89,26 +89,6 @@ class SortCollectionHandler extends OperationHandler {
     }
 }
 
-class ShowInfoHandler extends OperationHandler {
-    @Override
-    protected boolean canHandle(OperationLevel level) {
-        return level == OperationLevel.SHOW_INFO;
-    }
-
-    @Override
-    protected void processRequest() {
-        System.out.println("Метод сортировки: Быстрая сортировка (Quick sort).");
-        System.out.println("Размер коллекции: " + collectionSize);
-
-        StringBuilder msg = new StringBuilder("Коллекция");
-        if (students == null) msg.append(" не заполнена.");
-        else msg.append(" заполнена.");
-        System.out.println(msg.toString());
-
-        System.out.println("Поле сортировки ... "); // дописать - задано или нет.
-    }
-}
-
 class WriteToCSVFileHandler extends OperationHandler {
     @Override
     protected boolean canHandle(OperationLevel level) {
---
+++ src/main/java/org/astongrouph/operations/ShowInfoHandler.java
@@ -0,0 +1,21 @@
+package org.astongrouph.operations;
+
+class ShowInfoHandler extends OperationHandler {
+    @Override
+    protected boolean canHandle(OperationLevel level) {
+        return level == OperationLevel.SHOW_INFO;
+    }
+
+    @Override
+    protected void processRequest() {
+        System.out.println("Метод сортировки: Быстрая сортировка (Quick sort).");
+        System.out.println("Размер коллекции: " + collectionSize);
+
+        StringBuilder msg = new StringBuilder("Коллекция");
+        if (students == null) msg.append(" не заполнена.");
+        else msg.append(" заполнена.");
+        System.out.println(msg.toString());
+
+        System.out.println("Поле сортировки ... "); // дописать - задано или нет.
+    }
+}
